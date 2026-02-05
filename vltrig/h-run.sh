90bbedc3137c6307ff4b6bc89f5e68b33cf20f94
Viktor Komarov <41443179+Kpaccyc@users.noreply.github.com>
2026-02-05 12:11:23 +0300

    Update h-run.sh

--- vltrig/h-run.sh
+++ vltrig/h-run.sh
@@ -4,21 +4,20 @@ cd `dirname $0`
 . h-manifest.conf
 
 while true; do
-	for con in `netstat -anp | grep TIME_WAIT | grep $CUSTOM_API_PORT | awk '{print $5}'`; do
-		killcx $con lo
-	done
-	netstat -anp | grep TIME_WAIT | grep $CUSTOM_API_PORT &&
-		continue ||
-		break
+        for con in `netstat -anp | grep TIME_WAIT | grep $CUSTOM_API_PORT | awk '{print $5}'`; do
+                killcx $con lo
+        done
+        netstat -anp | grep TIME_WAIT | grep $CUSTOM_API_PORT &&
+                continue ||
+                break
 done
 
 
 wget https://github.com/Kpaccyc/Vltrig-HiveOS/blob/master/vltrig/control
 if [ $(cat control | grep -i "Version: $CUSTOM_VERSION" | wc -l) -eq 0 ];
-	then
-		mv $CUSTOM_CONFIG_FILENAME /hive/miners/custom/;
-		/hive/miners/custom/custom-get https://github.com/Kpaccyc/Vltrig-HiveOS/releases/download/Release/vltrig.tar.gz -f;
-		mv /hive/miners/custom/config.conf $CUSTOM_CONFIG_FILENAME;
+        then
+                mv $CUSTOM_CONFIG_FILENAME /hive/miners/custom/;
+                /hive/miners/custom/custom-get https://github.com/Kpaccyc/Vltrig-HiveOS/releases/download/Release/vltrig.tar.gz -f;
 fi
 rm -f control
 
@@ -28,10 +27,10 @@ then
   apt-get install libmicrohttpd-dev -y;
 fi
 
-cd /hive/miners/custom/$CUSTOM_NAME
-export GPU_FORCE_64BIT_PTR=1
-export GPU_USE_SYNC_OBJECTS=1
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_SINGLE_ALLOC_PERCENT=100
-export GPU_MAX_HEAP_SIZE=100
-./vltrig --log-file=$CUSTOM_LOG_BASENAME.log --api-port=$CUSTOM_API_PORT $(< $CUSTOM_CONFIG_FILENAME) $@ 2>&1
+# HugePages tunning
+function HugePagesTune(){
+          sysctl -w vm.nr_hugepages=0 > /dev/null
+          hugepages -erx || hugepages -rx
+}
+cd /hive/miners/custom
+./vltrig --log-file=$CUSTOM_LOG_BASENAME.log --http-port=$CUSTOM_API_PORT $(< $CUSTOM_CONFIG_FILENAME) $@ 2>&1
