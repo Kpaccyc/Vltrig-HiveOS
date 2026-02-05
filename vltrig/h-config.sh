8e0afc74d2b2f08ae054bf0eebaf49a9df599fa2
Viktor Komarov <41443179+Kpaccyc@users.noreply.github.com>
2026-02-05 12:10:36 +0300

    Update h-config.sh

--- vltrig/h-config.sh
+++ vltrig/h-config.sh
@@ -11,12 +11,11 @@ THIS_MINER_NAME="custom"
 [[ -z $CUSTOM_URL ]] && echo -e "${YELLOW}CUSTOM_URL is empty${NOCOLOR}" && return 1
 [[ -z $CUSTOM_ALGO ]] && echo -e "${YELLOW}CUSTOM_ALGO is empty${NOCOLOR}" && return 1
 
-conf=" -o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} ${CUSTOM_USER_CONFIG} -p ${CUSTOM_PASS}"
+function miner_config_gen() {
+        conf=" -o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} ${CUSTOM_USER_CONFIG} -p ${CUSTOM_PASS}"
+        [[ ! -z $USER_CONFIG ]] && conf=$(sed "s/%USER_CONFIG%/$USER_CONFIG/g" <<< "$conf") #|| echo "${RED}USER_CONFIG not set${NOCOLOR}"
+        [[ ! -z $WORKER_NAME ]] && conf=$(sed "s/%WORKER_NAME%/$WORKER_NAME/g" <<< "$conf") #|| echo "${RED}WORKER_NAME not set${NOCOLOR}"
 
-#replace tpl values in whole file
-[[ ! -z $USER_CONFIG ]] && conf=$(sed "s/%USER_CONFIG%/$USER_CONFIG/g" <<< "$conf") #|| echo "${RED}USER_CONFIG not set${NOCOLOR}"
-[[ ! -z $WORKER_NAME ]] && conf=$(sed "s/%WORKER_NAME%/$WORKER_NAME/g" <<< "$conf") #|| echo "${RED}WORKER_NAME not set${NOCOLOR}"
-
-[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
-echo "$conf" > $CUSTOM_CONFIG_FILENAME
-
+        [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
+        echo "$conf" > $CUSTOM_CONFIG_FILENAME
+}
